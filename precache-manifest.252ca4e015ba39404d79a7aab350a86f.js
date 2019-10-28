ab488e4c36cb69c023d62edbda892c4c866aa5e4
Vadim Demidov <v.demidov@muz-lab.ru>
2019-10-28 13:37:02 +0300

    Updates

---
+++ precache-manifest.252ca4e015ba39404d79a7aab350a86f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caeb0b4c4766e2a04c177866418e3e05",
+    "url": "/vista-test/index.html"
+  },
+  {
+    "revision": "d2ff34417d2f23e0d88d",
+    "url": "/vista-test/static/js/2.e71c3fe0.chunk.js"
+  },
+  {
+    "revision": "f5b76ecf6f9318681ea6",
+    "url": "/vista-test/static/js/main.5080431b.chunk.js"
+  },
+  {
+    "revision": "88a3fbc12e4b65784d4c",
+    "url": "/vista-test/static/js/runtime-main.9e6e38a8.js"
+  }
+]);
